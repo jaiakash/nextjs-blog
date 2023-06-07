07876d4eae72f395892fcdae9d1f393f94ac8333
Akash Jaiswal <akashjaiswal3846@gmail.com>
2023-06-08 00:59:29 +0530

    learn: linking pages

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
-import Head from 'next/head';
-import styles from '../styles/Home.module.css';
+import Head from "next/head";
+import Link from "next/link";
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
@@ -10,10 +11,10 @@ export default function Home() {
       </Head>
 
       <main>
-        <h1 className={styles.title}>
-          Learn <a href="https://nextjs.org">Next.js!</a>
+        <h1 className="title">
+          Read <Link href="/posts/first-post">this page!</Link>
         </h1>
-
+        
         <p className={styles.description}>
           Get started by editing <code>pages/index.js</code>
         </p>
@@ -55,7 +56,7 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
+          Powered by{" "}
           <img src="/vercel.svg" alt="Vercel" className={styles.logo} />
         </a>
       </footer>
@@ -111,5 +112,5 @@ export default function Home() {
         }
       `}</style>
     </div>
-  )
+  );
 }
